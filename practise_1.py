c77546c26525b2e4ecff242c2713afba88bdfbe4
dhrubo <147389051+dhrubo6768@users.noreply.github.com>
2023-11-30 09:27:16 +0600

    Add files via upload

--- practise_1.py
+++ practise_1.py
@@ -1,3 +1,28 @@
 n = "dhrubo"
 N = 7
 print(n, N)
+if 1 < 2:
+    print("5 is less than 2")
+    y = 5
+    b = 4
+else:
+    print("5 is greater than 2")
+next = str(3)
+print(type(int(float(next))))
+birds = ["Watermelon", "mango", "banana"]
+l ,k ,j = birds
+print(l + " " + k + " " + j)
+h = 5
+b = 6
+print(h + b)
+z = "than other subject"
+def myfunc():
+    global v
+    v = "very hard "
+myfunc()
+print("higher math is " + v + z)
+c = '''cratos is god of war
+his son is loki'''
+print(c[12])
+
+    
